07c41e020717b2b580c130297688dd16a25cc811
Burak Ülkü <brkulku@gmail.com>
2023-09-18 17:39:51 +0200

    Implement landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import './App.css';
-import logo from './logo.svg';
+import styles from './App.module.scss';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        main
+        <section>
+          <header>
+            <nav>
+              <a href="/#" Logo></a>
+              <ul>
+                <li>
+                  <a>Features</a>
+                </li>
+                <li>
+                  <a>Features</a>
+                </li>
+                <li>
+                  <a>Features</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </section>
+        <section>section2</section>
+      </main>
+      <footer>Footer</footer>
+    </>
   );
 }
