96a5c1ec1e80127da0179557ca4908022a509da7
Burak Ülkü <brkulku@gmail.com>
2023-09-19 16:57:15 +0200

    Land page clone

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ export default function App() {
   return (
     <>
       <main>
-        main
-        <section>
+        <section className={styles.heroSection}>
           <header>
             <nav>
               <a href="/#" Logo></a>
               <ul>
                 <li>
-                  <a>Features</a>
+                  <a>
+                    <h1>
+                      <span class="highlight"> &lt;h1&gt;</span>:wave: I'm
+                      &#123;name&#125;, an award-winning multidisciplinary
+                      designer based in &#123;location&#125;, with a focus on
+                      &#123;ability&#125; and a love of &#123;creative
+                      quirk&#125;<span class="highlight"> &lt;/h1&gt;</span>
+                    </h1>
+                  </a>
                 </li>
                 <li>
                   <a>Features</a>
@@ -20,6 +27,7 @@ export default function App() {
                   <a>Features</a>
                 </li>
               </ul>
+              <div></div>
             </nav>
           </header>
         </section>
